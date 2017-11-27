d221c0edfd7368dbf14d39d6de278cb10bc6b330
Jumpei Matsuda <j.matsuda.bb26th@gmail.com>
2017-11-27 20:17:43 +0900

    Allow method element to generate fields

--- processor/src/main/java/com/github/jmatsu/multipreference/processor/extension/Element.kt
+++ processor/src/main/java/com/github/jmatsu/multipreference/processor/extension/Element.kt
@@ -1,9 +1,8 @@
 package com.github.jmatsu.multipreference.processor.extension
 
 import javax.lang.model.element.Element
-import javax.lang.model.element.ExecutableElement
+import javax.lang.model.element.ElementKind
 import javax.lang.model.element.PackageElement
-import javax.lang.model.element.VariableElement
 
 val Element.name: String
     get() = when (this) {
@@ -13,14 +12,19 @@ val Element.name: String
 
 inline fun <reified T> Element.annotation(): T? where T : Annotation = getAnnotation(T::class.java)
 
-fun Element.asVariable(): VariableElement? = this as? VariableElement
-
-fun Element.asExecutable(): ExecutableElement? = this as? ExecutableElement
-
 val Element.getterName: String
     get() = "${if (asType().isBoolean) "is" else "get"}${name.toUpperCamel()}"
 
 val Element.setterName: String
     get() = "set${name.toUpperCamel()}"
+
+val Element.parameterName: String
+    get() {
+        return if (kind == ElementKind.METHOD) {
+            name.toLowerCamel().replace("^get".toRegex(), "")
+        } else {
+            name.toLowerCamel()
+        }
+    }
 
 fun Element.hasAnnotation(klass: Class<out Annotation>?): Boolean = klass?.let { getAnnotation(it) } != null
